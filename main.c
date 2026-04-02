5cf2175b1a3040f41c3f67d1c4572abad93069dc
JakeVandal <jake260van@gmail.com>
2026-04-02 10:49:19 -0500

    Refactor main.c for Teensy 4.1 and AS5045 support
    Updated includes for Teensy 4.1 framework and added AS5045 encoder initialization.

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
-#include<stdio.h>
-#include<math.h>
-#include<string.h>
+#include <Arduino.h>    // Core Teensy 4.1 / Teensyduino framework
+#include <SPI.h>        // SPI bus library
+#include <AS5045.h>     // AS5045 magnetic encoder library
+#include <stdint.h>     // Fixed-width integer types
+#include <stdbool.h>    // Boolean type and true/false constants
+#include <string.h>     // String manipulation functions
+#include <stdio.h>      // Standard I/O functions
 
-void main() {
-    int stuff = 1;
+// AS5045 chip select pin
+#define CS_PIN 10
+
+AS5045 encoder(CS_PIN);
+
+int main(void) {
+
+}
+
+void setup() {
+    
 }
